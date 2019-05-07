78128d4be4494a9b225afb8b0eeaaf3555f3d102
LotosPage <34channel@mail.ru>
2019-05-08 03:00:17 +0500

    Initial commit

---
+++ src/main/java/com/HomeWork/Page6.java
@@ -0,0 +1,27 @@
+package com.HomeWork;
+
+import java.util.Scanner;
+
+public class Page6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String secretWord = "sparrow";
+        String guess = "";
+        int guessCount = 0;
+        int guessLimit = 3;
+
+        while(!secretWord.equals(guess) && guessLimit>guessCount){
+            guessCount +=1;
+            System.out.println("Enter secret word: ");
+            guess = in.next();
+        }
+        if (secretWord.equals(guess)){
+            System.out.println("YOU WIN!!!");
+        }
+        else{
+            System.out.println("You loss!");
+        }
+    }
+}
+
+
